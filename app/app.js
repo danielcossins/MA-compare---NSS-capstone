10be26223b06538b551edca0fdcc3cdd7a6432ac
danielcossins11 <daniel.cossins@yahoo.com>
2015-09-12 11:55:27 -0500

    adding starting json files

---
+++ app/app.js
@@ -0,0 +1,13 @@
+var app = angular.module("MAApp",[ 'ngRoute', 'firebase']);
+
+app.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'partials/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
---
+++ app/controllers/MainCtrl.js
@@ -0,0 +1,12 @@
+app.controller("MainCtrl", 
+  ["$scope",
+   "$firebaseArray",
+  function($scope, $firebaseArray) {
+    // var ref = new Firebase("https://ma-compare.firebaseio.com/"); 
+
+    // Data from firebase 
+    // $scope.something = $firebaseArray(ref);
+
+    
+  }
+]);
