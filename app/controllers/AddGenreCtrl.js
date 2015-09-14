52870a5c030d1898d1401f06fc82fe863b24d1f6
danielcossins11 <daniel.cossins@yahoo.com>
2015-09-14 10:55:06 -0500

    populating all adds with selects

--- app/controllers/AddGenreCtrl.js
+++ app/controllers/AddGenreCtrl.js
@@ -3,13 +3,23 @@ app.controller("AddGenreCtrl",
    "$routeParams",
    "$firebaseArray",
    "$location",
-  function($scope,  $routeParams, $firebaseArray, $location) {
-    console.log("as;dkfl");
-    // var ref = new Firebase("https://ma-compare.firebaseio.com/arts"); 
+   "$http",
+  function($scope,  $routeParams, $firebaseArray, $location, $http) {
+    var ref = new Firebase("https://ma-compare.firebaseio.com/genres"); 
 
-    // // Data from firebase 
-    // $scope.arts = $firebaseArray(ref);
+    // Data from firebase 
+    $scope.genres = $firebaseArray(ref);
 
-    
+    $http.get("https://ma-compare.firebaseio.com/arts/.json").
+    then(function(data) {
+      console.log(data);
+      $scope.arts = data.data;
+    });
+
+    $http.get("https://ma-compare.firebaseio.com/techniques/.json").
+    then(function(data) {
+      console.log(data);
+      $scope.techniques = data.data;
+    });
   }
 ]);
--- app/controllers/AddTechCtrl.js
+++ app/controllers/AddTechCtrl.js
@@ -3,13 +3,23 @@ app.controller("AddTechCtrl",
    "$routeParams",
    "$firebaseArray",
    "$location",
-  function($scope,  $routeParams, $firebaseArray, $location) {
-    console.log("as;dkfl");
-    // var ref = new Firebase("https://ma-compare.firebaseio.com/arts"); 
+   "$http",
+  function($scope,  $routeParams, $firebaseArray, $location, $http) {
+    var ref = new Firebase("https://ma-compare.firebaseio.com/arts"); 
 
-    // // Data from firebase 
-    // $scope.arts = $firebaseArray(ref);
+    // Data from firebase 
+    $scope.techniques = $firebaseArray(ref);
 
-    
+    $http.get("https://ma-compare.firebaseio.com/arts/.json").
+    then(function(data) {
+      console.log(data);
+      $scope.arts = data.data;
+    });
+
+    $http.get("https://ma-compare.firebaseio.com/genres/.json").
+    then(function(data) {
+      console.log(data);
+      $scope.genres = data.data;
+    });
   }
 ]);
