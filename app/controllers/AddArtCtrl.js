2c8b3a4e32dac42d89a1c4cc8d70bb89bebb81a0
danielcossins11 <daniel.cossins@yahoo.com>
2015-09-14 14:03:41 -0500

    getting values from all checkboxes

--- app/controllers/AddArtCtrl.js
+++ app/controllers/AddArtCtrl.js
@@ -22,5 +22,42 @@ app.controller("AddArtCtrl",
       console.log(data);
       $scope.techniques = data.data;
     });
+
+
+
+
+    $scope.saveToFirebase = function(){
+      var art = {
+        name: angular.element("#name"),
+        description: angular.element("#description"),
+        image: angular.element("#image"),
+        genres: [
+          
+        ],
+        techniques: [
+          
+        ]
+      };
+      var genreBoxesArr = angular.element(".genres");
+      console.log(genreBoxesArr);
+      for(var i=0; i<genreBoxesArr.length; i++){
+        if(genreBoxesArr[i].checked===true){
+          console.log("this one matches", genreBoxesArr[i]);
+          art.genres.push(genreBoxesArr[i].value);
+        }else{
+          console.log("no match");
+        }
+      }
+      var techniqueBoxesArr = angular.element(".techniques");
+      console.log(techniqueBoxesArr);
+      for(var j=0; j<techniqueBoxesArr.length; j++){
+        if(techniqueBoxesArr[j].checked===true){
+          console.log("this one matches", techniqueBoxesArr[j]);
+          art.techniques.push(techniqueBoxesArr[j].value);
+        }else{
+          console.log("no match");
+        }
+      }
+    };
   }
 ]);
