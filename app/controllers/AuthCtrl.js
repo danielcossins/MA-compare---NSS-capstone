0ba78a110fa60a07eddd386362a2a0c841c76f49
danielcossins11 <daniel.cossins@yahoo.com>
2015-09-15 22:58:47 -0500

    removed some junk from auth control

--- app/controllers/AuthCtrl.js
+++ app/controllers/AuthCtrl.js
@@ -12,7 +12,6 @@ app.controller("AuthCtrl",
       "password": ""
     };
 
-    $scope.currentPicUrl = "";
 
 
     // Authenticates user to firebase data
@@ -25,32 +24,7 @@ app.controller("AuthCtrl",
     $scope.auth.$onAuth(function(authData) {
       console.log("authData", authData);
       //changes the profile picture based on how user is logged in
-      if(authData!==null){
-
-        //sets the user id in a factory for other controllers to use
-        storage.setUserId(authData.uid);
-        switch (authData.provider) {
-          case "facebook":
-            $scope.currentPicURL = authData.facebook.profileImageURL;
-            break;
-          case "twitter":
-            $scope.currentPicURL = authData.twitter.profileImageURL;
-            break;
-          case "password":
-            $scope.currentPicURL = authData.password.profileImageURL;
-            break;
-          case "github":
-            $scope.currentPicURL = authData.github.profileImageURL;
-            break;
-            case "google":
-            $scope.currentPicURL = authData.google.profileImageURL;
-            break;
-          default:
-            $scope.currentPicURL = "";
-        }
-      }else{
-        $scope.currentPicURL = "";
-      }
+      storage.setAuthData(authData);
       $scope.authData = authData;
       console.log($scope.authData);
     });
@@ -67,7 +41,6 @@ app.controller("AuthCtrl",
           // console.log("Login Failed!", error);
         } else {
           console.log("Authenticated successfully with payload:", authData);
-          storage.setUserId(authData.uid);
           storage.setAuthData(authData);
         }
       });
--- app/factories/storage.js
+++ app/factories/storage.js
@@ -1,15 +1,7 @@
 app.factory("storage", function(){
-  var userId = null;
   var authData;
 
   return {
-    getUserId: function() {
-      return userId;
-    },
-    setUserId: function(id) {
-      userId = id;
-      console.log("userId", userId);
-    },
 
     getAuthData: function(){
       return authData;
