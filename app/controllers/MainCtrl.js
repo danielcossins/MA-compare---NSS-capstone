0e573dc353b123f11f7fd8b91545908fccd84b58
danielcossins11 <daniel.cossins@yahoo.com>
2015-09-16 21:05:27 -0500

    added best technique to main page

--- app/controllers/MainCtrl.js
+++ app/controllers/MainCtrl.js
@@ -21,6 +21,29 @@ app.controller("MainCtrl",
       console.error(err);
     });
 
+
+
+
+    var ref2 = new Firebase("https://ma-compare.firebaseio.com/techniques"); 
+
+    // Data from firebase 
+    $scope.techs = $firebaseArray(ref2);
+    console.log($scope.techs);
+
+    $scope.techs.$loaded()
+    .then(function() {
+      console.log($scope.techs);
+      $scope.bestTech = $scope.techs[0];
+      for(var i=0; i<$scope.techs.length; i++){
+        if($scope.techs[i].votes > $scope.bestTech.votes){
+          $scope.bestTech = $scope.techs[i];
+        }
+      }
+    })
+    .catch(function(err) {
+      console.error(err);
+    });
+
     
   }
 ]);
