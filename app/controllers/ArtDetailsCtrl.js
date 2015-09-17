281b3499bc7d3fde22c79a85f769a46213768ea9
danielcossins11 <daniel.cossins@yahoo.com>
2015-09-17 11:10:35 -0500

    added users array to firebase

--- app/controllers/ArtDetailsCtrl.js
+++ app/controllers/ArtDetailsCtrl.js
@@ -3,7 +3,8 @@ app.controller("ArtDetailsCtrl",
    "$routeParams",
    "$firebaseArray",
    "$location",
-  function($scope,  $routeParams, $firebaseArray, $location) {
+   "$rootScope",
+  function($scope,  $routeParams, $firebaseArray, $location, $rootScope) {
     var ref = new Firebase("https://ma-compare.firebaseio.com/arts"); 
 
     // Data from firebase 
@@ -13,6 +14,10 @@ app.controller("ArtDetailsCtrl",
 
     var ref2 = new Firebase("https://ma-compare.firebaseio.com/genres"); 
     $scope.genres = $firebaseArray(ref2);
+
+    var ref3 = new Firebase("https://ma-compare.firebaseio.com/users"); 
+    $scope.users = $firebaseArray(ref3);
+    console.log($scope.users);
 
     //finds the correctly clicked art
     $scope.arts.$loaded()
@@ -33,6 +38,25 @@ app.controller("ArtDetailsCtrl",
 
     $scope.vote = function(){
       // $scope.clickedArt.votes = {};
+      // $scope.users = [];
+      var result = false;
+      for(var j=0; j<$scope.users.length; j++){
+        if($rootScope.user.uid===$scope.users[j].uid){
+          $scope.currentUser = $scope.users[j];
+          result=true;
+        }
+      }
+      if(result===false){
+        $scope.currentUser.uid = $rootScope.user.uid;
+        // $scope.users.push($scope.currentUser);
+      }
+      $scope.currentUser = {
+        uid: $rootScope.user.uid
+      };
+
+
+
+
       var votesArr = angular.element(".votes");
       for(var i=0; i<votesArr.length; i++){
         if(votesArr[i].checked){
@@ -43,8 +67,12 @@ app.controller("ArtDetailsCtrl",
             $scope.clickedArt.votes[votesArr[i].value]++;
           }
           console.log($scope.clickedArt);
+          $scope.currentUser[votesArr[i].value] = $scope.clickedArt.name;
         }
       }
+      console.log($scope.currentUser);
+      console.log($scope.users);
+      $scope.users.$add($scope.currentUser);
       $scope.arts.$remove($scope.clickedArt);
       $scope.arts.$add($scope.clickedArt);
     };
