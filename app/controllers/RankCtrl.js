53f8190bb8be27c0aa9e871a2c6d890c9ee2def4
danielcossins11 <daniel.cossins@yahoo.com>
2015-09-16 23:18:48 -0500

    logic works. display does not

--- app/controllers/RankCtrl.js
+++ app/controllers/RankCtrl.js
@@ -18,10 +18,48 @@ app.controller("RankCtrl",
           $scope.bestArt = $scope.arts[i];
         }
       }
+
+      $scope.genres.$loaded()
+      .then(function() {
+        console.log($scope.arts);
+        for(var k=0; k<$scope.genres.length; k++){
+          $scope[$scope.genres[k].name] = $scope.arts[0];
+          console.log("$scope."+[$scope.genres[k].name], $scope[$scope.genres[k].name]);
+          for(var j=0; j<$scope.arts.length; j++){
+            if($scope.arts[j].votes.Overall > $scope[$scope.genres[k].name].votes.Overall){
+              $scope[$scope.genres[k].name] = $scope.arts[j];
+            }
+          }
+          console.log("for "+$scope.genres[k].name, $scope[$scope.genres[k].name]);
+        }
+      })
+      .catch(function(err) {
+        console.error(err);
+      });
     })
     .catch(function(err) {
       console.error(err);
     });
+
+
+    // $scope.genres.$loaded()
+    // .then(function() {
+    //   console.log($scope.arts);
+    //   for(var k=0; k<$scope.genres.length; k++){
+    //     console.log($scope.arts[0]);
+    //     $scope[$scope.genres[k].name] = $scope.arts[0];
+    //     console.log("$scope."+[$scope.genres[k].name], $scope[$scope.genres[k].name]);
+    //     for(var i=0; i<$scope.arts.length; i++){
+    //       if($scope.arts[i].votes.Overall > $scope[$scope.genres[k].name].votes.Overall){
+    //         $scope[$scope.genres[k].name] = $scope.arts[i];
+    //       }
+    //     }
+    //     console.log("for "+$scope.genres[k].name, $scope[$scope.genres[k].name]);
+    //   }
+    // })
+    // .catch(function(err) {
+    //   console.error(err);
+    // });
 
     
   }
