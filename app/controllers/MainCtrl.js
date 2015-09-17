0502dcbef5ca88e2ef7f8561e6949b42ecd76ca4
danielcossins11 <daniel.cossins@yahoo.com>
2015-09-16 20:25:45 -0500

    basic functionality for best art

--- app/controllers/MainCtrl.js
+++ app/controllers/MainCtrl.js
@@ -2,10 +2,24 @@ app.controller("MainCtrl",
   ["$scope",
    "$firebaseArray",
   function($scope, $firebaseArray) {
-    // var ref = new Firebase("https://ma-compare.firebaseio.com/"); 
+    var ref = new Firebase("https://ma-compare.firebaseio.com/arts"); 
 
     // Data from firebase 
-    // $scope.something = $firebaseArray(ref);
+    $scope.arts = $firebaseArray(ref);
+
+    $scope.arts.$loaded()
+    .then(function() {
+      console.log($scope.arts);
+      $scope.bestArt = $scope.arts[0];
+      for(var i=0; i<$scope.arts.length; i++){
+        if($scope.arts[i].votes > $scope.bestArt.votes){
+          $scope.bestArt = $scope.arts[i];
+        }
+      }
+    })
+    .catch(function(err) {
+      console.error(err);
+    });
 
     
   }
