4209bb298cd9a0f8f56cd89e9ac941dc9fc79056
danielcossins11 <daniel.cossins@yahoo.com>
2015-09-24 23:22:18 -0500

    displaying profile pic in navbar

--- app/controllers/RootCtrl.js
+++ app/controllers/RootCtrl.js
@@ -2,10 +2,13 @@ app.controller("RootCtrl",
   ["$scope",
   "$rootScope",
   "$firebaseAuth",
+  "$firebaseArray",
   "$location",
-  function($scope, $rootScope, $firebaseAuth, $location) {
+  function($scope, $rootScope, $firebaseAuth, $firebaseArray, $location) {
     var ref = new Firebase("https://ma-compare.firebaseio.com/");
     $scope.auth = $firebaseAuth(ref);
+    var ref2 = new Firebase("https://ma-compare.firebaseio.com/users");
+    $scope.users = $firebaseArray(ref2);
 
     $scope.user = $rootScope.user;
 
@@ -16,5 +19,16 @@ app.controller("RootCtrl",
       $location.path('/#/');
       location.reload();
     };
+
+    $scope.users.$loaded()
+    .then(function(){
+      for(var i=0; i<$scope.users.length; i++){
+        console.log($scope.user.uid, $scope.users[i].uid);
+        if($scope.users[i].uid===$scope.user.uid){
+          $scope.currentUser = $scope.users[i];
+          console.log($scope.currentUser);
+        }
+      }
+    });
   }
 ]);
